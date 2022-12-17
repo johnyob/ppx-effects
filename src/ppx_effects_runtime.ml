14f2ac7534f63dfac063999ffbe12c26a9a2aa92
johnyob <ajo41@cam.ac.uk>
2022-12-17 22:06:09 +0000

    Update runtime to use `Stdlib.Effect`

--- src/ppx_effects_runtime.ml
+++ src/ppx_effects_runtime.ml
@@ -3,18 +3,4 @@
 
 let raise = Stdlib.raise
 
-open Stdlib.EffectHandlers
-open Stdlib.EffectHandlers.Deep
-
-type nonrec ('a, 'b) handler = ('a, 'b) handler = {
-  retc : 'a -> 'b;
-  exnc : exn -> 'b;
-  effc : 'c. 'c eff -> (('c, 'b) continuation -> 'b) option;
-}
-
-type nonrec 'a effect_handler = 'a effect_handler = {
-  effc : 'b. 'b eff -> (('b, 'a) continuation -> 'a) option;
-}
-
-let match_with : type a b c. (a -> b) -> a -> (b, c) handler -> c = match_with
-let try_with : type a b. (a -> b) -> a -> b effect_handler -> b = try_with
+include Stdlib.Effect
